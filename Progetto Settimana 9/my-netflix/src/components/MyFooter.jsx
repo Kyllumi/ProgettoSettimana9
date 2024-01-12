0421e3dbd2659695f08353ae3425b9dd90b561ce
Kleo <kleoruda9@gmail.com>
2024-01-12 18:30:57 +0100

    Rinominato classi Footer

--- Progetto Settimana 9/my-netflix/src/components/MyFooter.jsx
+++ Progetto Settimana 9/my-netflix/src/components/MyFooter.jsx
@@ -3,20 +3,20 @@ import React from 'react'
 export default function MyFooter() {
     return (
         <footer>
-            <div class="row text-center mt-5">
-                <div class="col-6 offset-3">
-                    <div class="row">
-                        <div class="col col-12 text-left mb-2">
-                            <i class="fa fa-facebook footer-icon"></i>
-                            <i class="fa fa-instagram footer-icon"></i>
-                            <i class="fa fa-twitter footer-icon"></i>
-                            <i class="fa fa-youtube footer-icon"></i>
+            <div className="row text-center mt-5">
+                <div className="col-6 offset-3">
+                    <div className="row">
+                        <div className="col col-12 text-left mb-2">
+                            <i className="fa fa-facebook footer-icon"></i>
+                            <i className="fa fa-instagram footer-icon"></i>
+                            <i className="fa fa-twitter footer-icon"></i>
+                            <i className="fa fa-youtube footer-icon"></i>
                         </div>
                     </div>
-                    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg 4">
-                        <div class="col">
-                            <div class="row">
-                                <div class="col col-12 footer-links text-start">
+                    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg 4">
+                        <div className="col">
+                            <div className="row">
+                                <div className="col col-12 footer-links text-start">
                                     <p>
                                         <a href="#a" alt="footer link">Audio and Subtitles</a>
                                     </p>
@@ -32,9 +32,9 @@ export default function MyFooter() {
                                 </div>
                             </div>
                         </div>
-                        <div class="col">
-                            <div class="row">
-                                <div class="col col-12 footer-links text-start">
+                        <div className="col">
+                            <div className="row">
+                                <div className="col col-12 footer-links text-start">
                                     <p>
                                         <a href="#a" alt="footer link">Audio Description</a>
                                     </p>
@@ -47,9 +47,9 @@ export default function MyFooter() {
                                 </div>
                             </div>
                         </div>
-                        <div class="col">
-                            <div class="row">
-                                <div class="col col-12 footer-links text-start">
+                        <div className="col">
+                            <div className="row">
+                                <div className="col col-12 footer-links text-start">
                                     <p>
                                         <a href="#a" alt="footer link">Help Center</a>
                                     </p>
@@ -62,9 +62,9 @@ export default function MyFooter() {
                                 </div>
                             </div>
                         </div>
-                        <div class="col">
-                            <div class="row">
-                                <div class="col col-12 footer-links text-start">
+                        <div className="col">
+                            <div className="row">
+                                <div className="col col-12 footer-links text-start">
                                     <p>
                                         <a href="#a" alt="footer link">Gift Cards</a>
                                     </p>
@@ -78,15 +78,15 @@ export default function MyFooter() {
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col col-12 text-start mb-2">
-                            <button type="button" class="btn btn-sm footer-button border rounded-0 mt-3 text-secondary scNet">
+                    <div className="row">
+                        <div className="col col-12 text-start mb-2">
+                            <button type="button" className="btn btn-sm footer-button border rounded-0 mt-3 text-secondary scNet">
                                 Service Code
                             </button>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col col-12 text-left mb-2 mt-2 copyright text-secondary text-start copyrightNetflix">
+                    <div className="row">
+                        <div className="col col-12 text-left mb-2 mt-2 copyright text-secondary text-start copyrightNetflix">
                             © 1997-2022 Netflix, Inc.
                         </div>
                     </div>
